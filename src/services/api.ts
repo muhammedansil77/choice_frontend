965a6f5f4dba667227a071bb741c75fb4e691506
ansilanuuu777 <muhammedansil7777@gmail.com>
2026-05-15 19:53:53 +0530

    Update API URL to secure domain

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://54.145.218.45:5000/api';
+const API_BASE_URL = 'https://api.anzil.online/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
